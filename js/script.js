d0ede646a5df34a164fcd696ac45e75c3134378a
Md. Sohag <144806986+codedesigncraft@users.noreply.github.com>
2024-01-12 20:16:55 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    const intro = document.getElementById("intro");
+
+    // Hide the section after 5 seconds
+    setTimeout(function() {
+        intro.classList.add("hidden");
+    }, 3000);
+});
+
+const btnNavEl = document.querySelector(".btn__mobile");
+const headerEl = document.querySelector(".sidebar");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
